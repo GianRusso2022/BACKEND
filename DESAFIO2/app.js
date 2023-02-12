c39d1cecfe4bc0acf63bc328cac70389d1835a48
GianRusso2022 <patocriv69@gmail.com>
2023-02-12 10:48:38 -0300

    back

---
+++ DESAFIO2/app.js
@@ -0,0 +1,25 @@
+import  ProductManager from "./class/ProductManager.js";
+
+const products = new ProductManager();
+
+// START
+
+// products.addProduct("title1", "description1", 2000, "img1", 1, 20)
+// products.addProduct("title2", "description2", 2000, "img2", 2, 20)
+// products.addProduct("title3", "description3", 2000, "img3", 3, 20)
+
+// products.getProducts()
+
+// products.getProductById(3)
+
+// products.deleteProductById(2)
+
+// products.updateProduct({
+//     title: 'title1',
+//     description: 'description1',
+//     price: 3000,
+//     img: 'img1',
+//     code: 1,
+//     stock: 20,
+//     id: 1
+// })
