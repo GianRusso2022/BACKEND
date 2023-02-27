4f1a0e0e0dd37c85c4f524f704ab4b4e584797e6
GianRusso2022 <patocriv69@gmail.com>
2023-02-26 22:35:06 -0300

    preentrega

---
+++ PRIMER-PREENTREGA/src/controllers/ProductManager.js
@@ -0,0 +1,67 @@
+import { promises as fs } from "fs"
+import { nanoid } from "nanoid"
+
+class ProductManager {
+    constructor() {
+        this.path = "./src//models/products.json"
+    }
+
+    readProducts = async () => {
+        let products = await fs.readFile(this.path, "utf-8")
+        return JSON.parse(products)
+    }
+    
+    writeProducts = async (product) => {
+        await fs.writeFile(this.path, JSON.stringify(product))
+    }
+
+    exist = async (id) => {
+        let products = await this.readProducts()
+        return products.find(p => p.id === id)
+    }
+
+    addProducts = async (product) => {
+        let ProductsOld = await this.readProducts()
+        product.id = nanoid()
+        let productAll = [...ProductsOld, product]
+        await this.writeProducts(productAll)
+        return "Prodcuto agregado"
+    }
+
+    getProducts = async () => {
+        return await this.readProducts()
+    }
+
+    getProductsById = async (id) => {
+        let productById = await this.exist(id)
+        if (!productById) return "Producto inexistente"
+        return productById
+    }
+
+
+    updateProducts = async (id, product) => {
+        let productById = await this.exist(id)
+        if (!productById) return "Producto inexistente"
+        await this.deleteProducts(id)
+        let productOld = await this.readProducts()
+        let products = [{ ...product, id: id }, ...productOld]
+        await this.writeProducts(products)
+        return "Producto actualizado"
+
+    }
+
+    deleteProducts = async (id) => {
+        let products = await this.readProducts()
+        let existProduct = products.some(p => p.id === id)
+        if (existProduct) {
+            let filterProducts = products.filter(p => p.id != id)
+            await this.writeProducts(filterProducts)
+            return "Prodcuto eliminado"
+        }
+        return "Producto inexistente"
+    }
+
+}
+
+
+export default ProductManager
